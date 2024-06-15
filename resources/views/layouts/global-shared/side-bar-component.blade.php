a532eac1c3ce7b307c2a88d28d1f3b57d1180c0b
Ivan <ivan@luxodev.com>
2024-06-15 19:00:06 +0700

    initial commit

--- resources/views/layouts/global-shared/side-bar-component.blade.php
+++ resources/views/layouts/global-shared/side-bar-component.blade.php
@@ -12,6 +12,71 @@
                         </div>
                     </a>
                 </li>
+                <li>
+                    <a href="" class="side-menu {{request()->routeIs('') ? 'side-menu--active' : null}}">
+                        <div class="side-menu__icon">
+                            <i icon-name="pencil"></i>
+                        </div>
+                        <div class="side-menu__title">
+                            Master Data
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="side-menu">
+                        <div class="side-menu__icon">
+							<i icon-name="book"></i>
+                        </div>
+                        <div class="side-menu__title">
+                            Eksplorasi Data
+                            <div class="side-menu__sub-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="chevron-down" class="lucide lucide-chevron-down stroke-1.5 w-5 h-5"><path d="m6 9 6 6 6-6"></path></svg>
+                            </div>
+                        </div>
+                    </a>
+                    <ul class="">
+                    <li>
+                            <a href="" class="side-menu {{ request()->routeIs('') ? 'side-menu--active' : '' }}">
+                                <div class="side-menu__icon">
+                                    <i icon-name="book-open"></i>
+                                </div>
+                                <div class="side-menu__title">
+                                    Transaksi
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="" class="side-menu {{ request()->routeIs('') ? 'side-menu--active' : '' }}">
+                                <div class="side-menu__icon">
+                                    <i icon-name="book-open"></i>
+                                </div>
+                                <div class="side-menu__title">
+                                    Produk Terjual
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="" class="side-menu {{request()->routeIs('') ? 'side-menu--active' : null}}">
+                        <div class="side-menu__icon">
+                            <i icon-name="pencil"></i>
+                        </div>
+                        <div class="side-menu__title">
+                           FP-Growth
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="side-menu {{request()->routeIs('') ? 'side-menu--active' : null}}">
+                        <div class="side-menu__icon">
+                            <i icon-name="pencil"></i>
+                        </div>
+                        <div class="side-menu__title">
+                           Keluar
+                        </div>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
